5e2c76968fc977d6e727c3644d1bf65faabdeac0
George Stagg <george.stagg@newcastle.ac.uk>
2019-07-02 12:25:29 +0100

    Simplify float packing

--- gpe.js
+++ gpe.js
@@ -1,3 +1,4 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function getShader ( gl, id ){
     var shaderScript = document.getElementById ( id );
     var str = "";
@@ -18,6 +19,44 @@ function getShader ( gl, id ){
         alert(gl.getShaderInfoLog(shader));
     return shader;
 }
+var c = document.getElementById("GPE");
+var gl = c.getContext("webgl");
+var prog  = gl.createProgram();
+gl.attachShader(prog, getShader( gl, "shader-vs" ));
+gl.attachShader(prog, getShader( gl, "shader-fs-dpsi" ));
+gl.linkProgram(prog);
+
+var prog_step  = gl.createProgram();
+gl.attachShader(prog_step, getShader( gl, "shader-vs" ));
+gl.attachShader(prog_step, getShader( gl, "shader-fs-step" ));
+gl.linkProgram(prog_step);
+
+var prog_show  = gl.createProgram();
+gl.attachShader(prog_show, getShader( gl, "shader-vs" ));
+gl.attachShader(prog_show, getShader( gl, "shader-fs-show" ));
+gl.linkProgram(prog_show);
+
+gl.useProgram(prog);
+var aPosLoc = gl.getAttribLocation(prog, "aPos");
+var aTexLoc = gl.getAttribLocation(prog, "aTexCoord");
+gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-1,-1, 0,0,  1,-1, 1,0,  -1,1, 0,1, 1,1, 1,1]), gl.STATIC_DRAW);
+gl.vertexAttribPointer(aPosLoc, 2, gl.FLOAT, gl.FALSE, 16, 0);
+gl.vertexAttribPointer(aTexLoc, 2, gl.FLOAT, gl.FALSE, 16, 8);
+gl.enableVertexAttribArray( aPosLoc );
+gl.enableVertexAttribArray( aTexLoc );
+
+gl.useProgram(prog_step);
+var aPosLoc_step = gl.getAttribLocation(prog_step, "aPos");
+var aTexLoc_step = gl.getAttribLocation(prog_step, "aTexCoord");
+gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-1,-1, 0,0,  1,-1, 1,0,  -1,1, 0,1, 1,1, 1,1]), gl.STATIC_DRAW);
+gl.vertexAttribPointer(aPosLoc_step, 2, gl.FLOAT, gl.FALSE, 16, 0);
+gl.vertexAttribPointer(aTexLoc_step, 2, gl.FLOAT, gl.FALSE, 16, 8);
+gl.enableVertexAttribArray( aPosLoc_step );
+gl.enableVertexAttribArray( aTexLoc_step );
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 function draw(){
     for(var i=0; i<10;i++){
@@ -84,61 +123,22 @@ function draw(){
     requestAnimationFrame(draw);
 }
 
-var c = document.getElementById("GPE");
-var gl = c.getContext("webgl");
-var prog  = gl.createProgram();
-gl.attachShader(prog, getShader( gl, "shader-vs" ));
-gl.attachShader(prog, getShader( gl, "shader-fs-dpsi" ));
-gl.linkProgram(prog);
-
-var prog_step  = gl.createProgram();
-gl.attachShader(prog_step, getShader( gl, "shader-vs" ));
-gl.attachShader(prog_step, getShader( gl, "shader-fs-step" ));
-gl.linkProgram(prog_step);
-
-var prog_show  = gl.createProgram();
-gl.attachShader(prog_show, getShader( gl, "shader-vs" ));
-gl.attachShader(prog_show, getShader( gl, "shader-fs-show" ));
-gl.linkProgram(prog_show);
-
-gl.useProgram(prog);
-var aPosLoc = gl.getAttribLocation(prog, "aPos");
-var aTexLoc = gl.getAttribLocation(prog, "aTexCoord");
-gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-1,-1, 0,0,  1,-1, 1,0,  -1,1, 0,1, 1,1, 1,1]), gl.STATIC_DRAW);
-gl.vertexAttribPointer(aPosLoc, 2, gl.FLOAT, gl.FALSE, 16, 0);
-gl.vertexAttribPointer(aTexLoc, 2, gl.FLOAT, gl.FALSE, 16, 8);
-gl.enableVertexAttribArray( aPosLoc );
-gl.enableVertexAttribArray( aTexLoc );
-
-gl.useProgram(prog_step);
-var aPosLoc_step = gl.getAttribLocation(prog_step, "aPos");
-var aTexLoc_step = gl.getAttribLocation(prog_step, "aTexCoord");
-gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-1,-1, 0,0,  1,-1, 1,0,  -1,1, 0,1, 1,1, 1,1]), gl.STATIC_DRAW);
-gl.vertexAttribPointer(aPosLoc_step, 2, gl.FLOAT, gl.FALSE, 16, 0);
-gl.vertexAttribPointer(aTexLoc_step, 2, gl.FLOAT, gl.FALSE, 16, 8);
-gl.enableVertexAttribArray( aPosLoc_step );
-gl.enableVertexAttribArray( aTexLoc_step );
-
 var n = 256;
 var dx = 0.5;
 var gamma = 0.01;
 var dt = 0.1;
 var pot_r = 3;
-var n1 = n-1, h = 1/n1, t = 0;
-pix = new Uint8Array(4*n*n);
-var p = 0;
-for(var i = 0; i < n; i++ )
-for(var j = 0; j < n; j++ ){
-    var x1 = (j - n/2)*dx - 5., y1 = (i - n/2)*dx, fi1 = Math.atan2(y1,x1);
-    var x2 = (j - n/2)*dx + 5., y2 = (i - n/2)*dx, fi2 = Math.atan2(y2,x2);
-    var r1 =  1 - Math.exp(-2.*(x1*x1 + y1*y1));
-    var r2 =  1 - Math.exp(-2.*(x2*x2 + y2*y2));
-    pix[p++] = 0;//x = r1*r2*Math.cos(fi1+fi2);
-    pix[p++] = 0;//y = r1*r2*Math.sin(fi1+fi2);
-    pix[p++] = 0;//Math.sqrt(x*x + y*y);
-    pix[p++] = 0;
+var pix = new Uint8Array(4*n*n);
+
+function setReflective(gl_tex){
+    gl.activeTexture(gl_tex);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.MIRRORED_REPEAT);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT);
+}
+function setPeriodic(gl_tex){
+    gl.activeTexture(gl_tex);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
 }
 
 function makeTexture(gl_tex){
@@ -149,8 +149,7 @@ function makeTexture(gl_tex){
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, n, n, 0, gl.RGBA, gl.UNSIGNED_BYTE, pix);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.MIRRORED_REPEAT);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT);
+    setReflective(gl_tex);
     return t;
 }
 
@@ -203,10 +202,8 @@ gl.uniform1f(gl.getUniformLocation(prog_step, "addVortex_x"), 0);
 gl.uniform1f(gl.getUniformLocation(prog_step, "addVortex_y"), 0);
 gl.uniform1i(gl.getUniformLocation(prog_step, "reset"), 1);
 
-
 gl.useProgram(prog_show);
 gl.uniform1i(gl.getUniformLocation(prog_show, "psi"), 1);
-
 
 var scale = ((window.innerHeight<window.innerWidth)?window.innerHeight/1.3:window.innerWidth)/1.2;
 $("#GPE").css("height",scale);
@@ -222,10 +219,10 @@ c.addEventListener("touchend", function (e) {
 c.addEventListener("touchmove", function (e) {
     var touch = e.touches[0];
     var rect = c.getBoundingClientRect();
-    gl.useProgram(prog);
-    gl.uniform1i(gl.getUniformLocation(prog, "addPot"), 1);
     var x =  touch.clientX - rect.left;
     var y =  touch.clientY - rect.top;
+    gl.useProgram(prog);
+    gl.uniform1i(gl.getUniformLocation(prog, "addPot"), 1);
     gl.uniform1f(gl.getUniformLocation(prog, "addPot_x"), x/scale);
     gl.uniform1f(gl.getUniformLocation(prog, "addPot_y"), (scale-y)/scale);
 }, false);
@@ -253,20 +250,20 @@ $("#GPE")
     gl.useProgram(prog);
     gl.uniform1i(gl.getUniformLocation(prog, "addPot"), 0);
     if (!wasDragging) {
-        gl.useProgram(prog_step);
-        gl.uniform1i(gl.getUniformLocation(prog_step, "addVortex"), 1);
         var x = (e.offsetX != null) ? e.offsetX : e.originalEvent.layerX;
         var y = (e.offsetY != null) ? e.offsetY : e.originalEvent.layerY;
+        gl.useProgram(prog_step);
+        gl.uniform1i(gl.getUniformLocation(prog_step, "addVortex"), 1);
         gl.uniform1f(gl.getUniformLocation(prog_step, "addVortex_x"), x/scale);
         gl.uniform1f(gl.getUniformLocation(prog_step, "addVortex_y"), (scale-y)/scale);
     }
 });
 $("#GPE").contextmenu(function(e) {
     isDragging = true;
-    gl.useProgram(prog_step);
-    gl.uniform1i(gl.getUniformLocation(prog_step, "addVortex"), -1);
     var x = (e.offsetX != null) ? e.offsetX : e.originalEvent.layerX;
     var y = (e.offsetY != null) ? e.offsetY : e.originalEvent.layerY;
+    gl.useProgram(prog_step);
+    gl.uniform1i(gl.getUniformLocation(prog_step, "addVortex"), -1);
     gl.uniform1f(gl.getUniformLocation(prog_step, "addVortex_x"), x/scale);
     gl.uniform1f(gl.getUniformLocation(prog_step, "addVortex_y"), (scale-y)/scale);
     return false;
@@ -274,8 +271,8 @@ $("#GPE").contextmenu(function(e) {
 
 var gui = new dat.GUI();
 var boundary = 'reflective';
-var gammaController = gui.add(window, 'gamma',0,0.5).step(0.001);
-var dtController = gui.add(window, 'dt',0,0.2).step(0.01);
+var gammaController = gui.add(window, 'gamma',0,0.3).step(0.001);
+var dtController = gui.add(window, 'dt',0,0.14).step(0.01);
 var pot_rController = gui.add(window, 'pot_r',0,30).step(1);
 var boundaryController = gui.add(window, 'boundary', ['periodic', 'reflective']);
 
@@ -303,67 +300,19 @@ pot_rController.onChange(function(value) {
 
 boundaryController.onChange(function(value) {
     if(value == 'periodic'){
-        gl.activeTexture(gl.TEXTURE0);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
-        gl.activeTexture(gl.TEXTURE1);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
-        gl.activeTexture(gl.TEXTURE2);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
-        gl.activeTexture(gl.TEXTURE3);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
-        gl.activeTexture(gl.TEXTURE4);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
-        gl.activeTexture(gl.TEXTURE5);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+        setPeriodic(gl.TEXTURE0);
+        setPeriodic(gl.TEXTURE1);
+        setPeriodic(gl.TEXTURE2);
+        setPeriodic(gl.TEXTURE3);
+        setPeriodic(gl.TEXTURE4);
+        setPeriodic(gl.TEXTURE5);
     } else { 
-        gl.activeTexture(gl.TEXTURE0);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.MIRRORED_REPEAT);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT);
-        gl.activeTexture(gl.TEXTURE1);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.MIRRORED_REPEAT);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT);
-        gl.activeTexture(gl.TEXTURE2);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.MIRRORED_REPEAT);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT);
-        gl.activeTexture(gl.TEXTURE3);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.MIRRORED_REPEAT);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT);
-        gl.activeTexture(gl.TEXTURE4);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.MIRRORED_REPEAT);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT);
-        gl.activeTexture(gl.TEXTURE5);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.MIRRORED_REPEAT);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT);
+        setReflective(gl.TEXTURE0);
+        setReflective(gl.TEXTURE1);
+        setReflective(gl.TEXTURE2);
+        setReflective(gl.TEXTURE3);
+        setReflective(gl.TEXTURE4);
+        setReflective(gl.TEXTURE5);
     }
 });
   
